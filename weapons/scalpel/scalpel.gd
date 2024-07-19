4afeb8aea44b457c3992f144d643708c76aa3a84
Makaravičius Lukas <lukas.makaravicius@mif.stud.vu.lt>
2024-07-19 23:38:58 +0300

    t :Add scalpel

--- weapons/scalpel/scalpel.gd
+++ weapons/scalpel/scalpel.gd
@@ -1,5 +1,25 @@
 extends Area2D
 
+var enemy = null
+var enemy_detected = false
+
+func _physics_process(delta):
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemy = enemies_in_range.front()
+		look_at(target_enemy.global_position)
+
 func _on_stab_frequency_timeout():
-	%AnimationPlayer.play("stab")
 	
+	if enemy_detected == true && enemy != null:
+		%AnimationPlayer.play("stab")
+		%StabSound.play()
+		enemy.take_damage()
+	
+func _on_stab_area_body_entered(body):
+	print(body.name)
+	if body.has_method("take_damage"):
+		enemy_detected = true
+		enemy = body
+	else:
+		enemy_detected = false
